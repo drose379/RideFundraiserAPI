50e35fee2c8821301abdafa7f894654262dc1ee6
drose379 <dylanrose60@DylanRosMacBook.home>
2015-07-07 21:54:19 -0400

    added connect object, working on getting user input

--- newLiveEvent.php
+++ newLiveEvent.php
@@ -1,7 +1,15 @@
 <?php
 
+require 'connect.php';
+
 class LiveEvent {
 	public function run() {
-		echo "Made it!";
+		$post = json_decode(file_get_contents('php://input'),true);
+
+		var_dump($post);
+	}
+
+	public function createLiveEvent($user,$event,$organization,$donationRate,$distanceGoal) {
+
 	}
 }
