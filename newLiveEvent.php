2a1d2390bf38035a16925059f544d78ffbe8a29e
drose379 <dylanrose60@DylanRosMacBook.home>
2015-07-11 09:20:55 -0400

    testing grabbing variables from POST

--- newLiveEvent.php
+++ newLiveEvent.php
@@ -5,11 +5,17 @@ require 'connect.php';
 class LiveEvent {
 	public function run() {
 		$post = json_decode(file_get_contents('php://input'),true);
+		
+		$user = $post[0];
+		$eventName = $post[1];
+		$organization = $post[2];
+		$rate = $post[3];
+		$goalDistance = $post[4];
 
-		var_dump($post);
+		$this->createLiveEvent($user,$eventName,$organization,$rate,$goalDistance);
 	}
 
 	public function createLiveEvent($user,$event,$organization,$donationRate,$distanceGoal) {
-
+		echo "Creating live event called on" + $user;
 	}
 }
