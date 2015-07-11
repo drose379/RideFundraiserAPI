a79fd5dff69ac29b7d976f568776a4bc4de084fd
drose379 <dylanrose60@DylanRosMacBook.home>
2015-07-11 10:06:13 -0400

    testing vars

--- newLiveEvent.php
+++ newLiveEvent.php
@@ -7,14 +7,14 @@ class LiveEvent {
 		$post = json_decode(file_get_contents('php://input'),true);
 		
 		$user = $post[0];
-		/*
 		$eventName = $post[1];
+		/*
 		$organization = $post[2];
 		$rate = $post[3];
 		$goalDistance = $post[4];
 		*/
 
-		echo $user;
+		echo $organization;
 
 		//$this->createLiveEvent($user,$eventName,$organization,$rate,$goalDistance);
 	}
