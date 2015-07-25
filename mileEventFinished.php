69ad02eb9ff9492aba1c68df0f785823b8bebd8e
Dylan <dylanrose60@gmail.com>
2015-07-25 10:39:13 -0400

    adding finished event functionality

--- mileEventFinished.php
+++ mileEventFinished.php
@@ -7,6 +7,7 @@
 		  * Remove Item from LiveMile table
 		  * Remove Item from LiveMileUpdates table
 		  * Pull data from LiveEventDonations table and form a JSONArray of donation summary, echo as string back to application 
+		  * Also echo all event data back to application (dist,avg speed,time, etc)
 		  */
 
 
@@ -21,7 +22,8 @@ class MileEventFinished {
 		$event = $post[1];
 
 		$eventID = $this->grabEventId($user,$event);
-		$this->moveFromLiveToComplete($eventID);
+		$this->moveFromLiveToComplete($eventID,$user,$event);
+		$this->generateDonationSummary($eventID);
 	}
 
 	public function grabEventId($user,$event) {
@@ -38,7 +40,7 @@ class MileEventFinished {
 		return $eventId;
 	}
 
-	public function moveFromLiveToComplete($eventId) {
+	public function moveFromLiveToComplete($eventId,$user,$eventName) {
 		//grab data from Live table
 		//insert into completed table
 
@@ -51,8 +53,35 @@ class MileEventFinished {
 		while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$eventInfo[] = $result;
 		}
-		echo json_encode($eventInfo);
+		
+		//use eventInfo to insert record into CompleteMileEvent table
+		$stmt2 = $con->prepare("INSERT INTO CompleteMileEvent (ID,user,eventName,distance,time,averageSpeed,goalReached) VALUES 
+			(:id,:user,:event,:distance,:time,:averageSpeed,:goalReached)");
+		$stmt2->bindParam(':id',$eventInfo["ID"]);
+		$stmt2->bindParam(':user',$user);
+		$stmt2->bindParam(':event',$eventName);
+		$stmt2->bindParam(':distance',$eventInfo["distance"]);
+		$stmt2->bindParam(':time',$eventInfo["time"]);
+		$stmt2->bindParam(':averageSpeed',$eventInfo["averageSpeed"]);
+		$stmt2->bindParam(':goalReached',$eventInfo["percentReached"]);
 	}
 
+	//generate donation summary and insert it into CompleteMileEvent record where id = $eventId, use UPDATE
+	public function generateDonationSummary($eventId) {
+		$donationParent = [];
+		
+		$con = DBConnect::get();
+		$stmt = $con->prepare("SELECT * FROM LiveEventDonations WHERE eventId = :id");
+		$stmt->bindParam(':id',$eventId);
+		$stmt->execute();
+		while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			$donationParent[] = $result;
+		}
+
+		echo json_encode($donationParent);
+
+	}
+
+	//cleanup, remove from LiveEvent,LiveEventUpdates and LiveEventDonations
 
 }
